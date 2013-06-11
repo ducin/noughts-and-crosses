9b134704641e063bf9397ce84370b6737261b069
Andriy Mykulyak <andriy@stasyk.(none)>
2013-06-11 07:38:20 +0200

    dobre praktyki - a) definiuemy main() przekazujac do niej argumenty wiersza polecen, b) sprawdzamy czy modul jest importowany czy wykonywany bezposrednio c) gwarantowanie zamykamy socket nawet jezeli wystapi jakis wyjatek

--- nac/client.py
+++ nac/client.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python
 
 import socket
+import sys
+
 import config
 from game import Game
 
-print 'Noughts and crosses, v1.0'
-print 'Connecting to', config.host, config.port
-s = socket.socket()
-s.connect((config.host, config.port))
+def main(args):
+	print 'Noughts and crosses, v1.0'
+	print 'Connecting to', config.host, config.port
+	s = socket.socket()
+	try:
+		s.connect((config.host, config.port))
 
-game = Game()
-game.display()
-while game.continues():
-	print 'Waiting for opponent move'
-	his_move = s.recv(1)
-	print 'Opponent >>', his_move
-	game.set(int(his_move))
-	game.display()
-	if not game.continues():
-		break
-	my_move = raw_input('Your move: ')
-	while not game.set(int(my_move)):
-		'This field cannot be marked.'
-		my_move = raw_input('Your move: ')
-	game.display()
-	s.send(my_move)
-print game.winner if game.winner else 'Nobody', 'has won!'
-s.close()
-print 'Connection closed'
+		game = Game()
+		game.display()
+		while game.continues():
+			print 'Waiting for opponent move'
+			his_move = s.recv(1)
+			print 'Opponent >>', his_move
+			game.set(int(his_move))
+			game.display()
+			if not game.continues():
+				break
+			my_move = raw_input('Your move: ')
+			while not game.set(int(my_move)):
+				'This field cannot be marked.'
+				my_move = raw_input('Your move: ')
+			game.display()
+			s.send(my_move)
+		print game.winner if game.winner else 'Nobody', 'has won!'
+	finally:
+		s.close()
+		print 'Connection closed'
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv[1:]))
--- nac/server.py
+++ nac/server.py
@@ -1,35 +1,43 @@
 #!/usr/bin/python
 
 import socket
+import sys
+
 import config
 from game import Game
 
-s = socket.socket()
-s.bind((config.host, config.port))
-s.listen(5)
+def main(args):
+	s = socket.socket()
+	try:
+		s.bind((config.host, config.port))
+		s.listen(5)
 
-print 'Noughts and crosses, v1.0'
-print 'Server started! Waiting for clients...'
+		print 'Noughts and crosses, v1.0'
+		print 'Server started! Waiting for clients...'
 
-c, addr = s.accept()
-print 'Got connection from', addr
+		c, addr = s.accept()
+		print 'Got connection from', addr
 
-game = Game()
-game.display()
-while game.continues():
-	my_move = raw_input('Your move: ')
-	while not game.set(int(my_move)):
-		print 'This field cannot be marked.'
-		my_move = raw_input('Your move: ')
-	game.display()
-	c.send(my_move)
-	if not game.continues():
-		break
-	print 'Waiting for opponent move'
-	his_move = c.recv(1024)
-	print 'Opponent >> ', his_move
-	game.set(int(his_move))
-	game.display()
-print game.winner if game.winner else 'Nobody', 'has won!'
-c.close()
-print 'Connection closed'
+		game = Game()
+		game.display()
+		while game.continues():
+			my_move = raw_input('Your move: ')
+			while not game.set(int(my_move)):
+				print 'This field cannot be marked.'
+				my_move = raw_input('Your move: ')
+			game.display()
+			c.send(my_move)
+			if not game.continues():
+				break
+			print 'Waiting for opponent move'
+			his_move = c.recv(1024)
+			print 'Opponent >> ', his_move
+			game.set(int(his_move))
+			game.display()
+		print game.winner if game.winner else 'Nobody', 'has won!'
+	finally:
+		s.close()
+		print 'Connection closed'
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv[1:]))
